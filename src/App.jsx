039814e69c2a454f553f88da3debedce9048bf41
Jose Garcia <josegarcia19809@gmail.com>
2024-01-09 20:57:39 -0600

    Control de presupuesto en React: Creando el Formulario de Presupuesto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Header from "./components/Header.jsx";
 import NuevoPresupuesto from "./components/NuevoPresupuesto.jsx";
+import {useState} from "react";
 
 function App() {
+    const [presupuesto, setPresupuesto] = useState(0);
     return (
         <>
-            <Header/>
-            <NuevoPresupuesto/>
+            <Header
+                presupuesto={presupuesto}
+                setPresupuesto={setPresupuesto}
+            />
         </>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
-const Header = () => {
+import NuevoPresupuesto from "./NuevoPresupuesto.jsx";
+
+const Header = ({presupuesto, setPresupuesto}) => {
     return (
         <header>
             <h1>Planificador de gastos</h1>
+            <NuevoPresupuesto
+                presupuesto={presupuesto}
+                setPresupuesto={setPresupuesto}
+            />
         </header>
+
     );
 };
 
