b72f7fcdf0651e5cc8621d44ae9265d8cc52fc6e
Jose Garcia <josegarcia19809@gmail.com>
2024-01-14 15:51:48 -0600

    Control de presupuesto en React: Añadiendo el Formulario de Gastos

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -21,6 +21,41 @@ const Modal = ({setModal, animarModal, setAnimarModal}) => {
 
             <form className={`formulario ${animarModal ? "animar" : "cerrar"}`}>
                 <legend>Nuevo gasto</legend>
+                <div className="campo">
+                    <label htmlFor="nombre">Nombre Gasto</label>
+                    <input
+                        id="nombre"
+                        type="text"
+                        placeholder="Añade el nombre del gasto"
+                    />
+                </div>
+                <div className="campo">
+                    <label htmlFor="cantidad">Cantidad</label>
+                    <input
+                        id="cantidad"
+                        type="number"
+                        placeholder="Añade la cantidad del gasto: p.ej. 300"
+                    />
+                </div>
+
+                <div className="campo">
+                    <label htmlFor="categoria">Categoría</label>
+                    <select id="categoria">
+                        <option value="">-- Seleccione --</option>
+                        <option value="ahorro">Ahorro</option>
+                        <option value="comida">Comida</option>
+                        <option value="casa">Casa</option>
+                        <option value="gastos">Gastos</option>
+                        <option value="ocio">Ocio</option>
+                        <option value="salud">Salud</option>
+                        <option value="suscripciones">Suscripciones</option>
+                        <option value=""></option>
+                    </select>
+                </div>
+                <input
+                    type="submit"
+                    value="Añadir gasto"
+                />
             </form>
         </div>
     );
