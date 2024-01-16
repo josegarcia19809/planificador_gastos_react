0bacfd4a6987046ae6b8eb0ec5c680b213d6eecb
Jgarcia <josegarcia19809@gmail.com>
2024-01-16 07:29:49 -0600

    Control de presupuesto en React: Formateando las Fechas

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,6 +1,8 @@
+import {formatearFecha} from "../helpers/index.js";
+
 const Gasto = ({gasto}) => {
 
-    const {categoria, nombre, cantidad, id} = gasto;
+    const {categoria, nombre, cantidad, id, fecha} = gasto;
 
     return (
         <div className="gasto sombra">
@@ -8,8 +10,13 @@ const Gasto = ({gasto}) => {
                 <div className="descripcion-gasto">
                     <p className="categoria">{categoria}</p>
                     <p className="nombre-gasto">{nombre}</p>
+                    <p className="fecha-gasto">
+                        Agregado el {""}
+                        <span>{formatearFecha(fecha)}</span>
+                    </p>
                 </div>
             </div>
+            <p className="cantidad-gasto">${cantidad}</p>
         </div>
     );
 };
