b7107c93151d1b6ceaae4cb3828e300f6226ed08
Jgarcia <josegarcia19809@gmail.com>
2024-01-16 10:04:43 -0600

    Control de presupuesto en React: Finalizando el Efecto de Swipe

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -30,19 +30,27 @@ const Gasto = ({gasto}) => {
 
     const {categoria, nombre, cantidad, id, fecha} = gasto;
     
-    const leadingActions = () => {
-      
-    }
+    const leadingActions = () => (
+        <LeadingActions>
+            <SwipeAction onClick={()=>console.log("Editar")}>
+                Editar
+            </SwipeAction>
+        </LeadingActions>
+    )
     
-    const trailingActions = () => {
-      
-    }
+    const trailingActions = () => (
+        <TrailingActions>
+            <SwipeAction onClick={()=>console.log("Eliminar")}>
+                Eliminar
+            </SwipeAction>
+        </TrailingActions>
+    )
 
     return (
         <SwipeableList>
             <SwipeableListItem
-            leadingActions={leadingActions}
-            trailingActions={trailingActions}
+            leadingActions={leadingActions()}
+            trailingActions={trailingActions()}
             >
                 <div className="gasto sombra">
                     <div className="contenido-gasto">
