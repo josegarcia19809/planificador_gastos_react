19ed299c0db3fce96a0068b3bc894907a1e18c78
Jgarcia <josegarcia19809@gmail.com>
2024-01-17 09:01:25 -0600

    Control de presupuesto en React: Colocar los Gastos en Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,12 @@ function App() {
     const [presupuesto, setPresupuesto] = useState(
         Number(localStorage.getItem("presupuesto")) ?? 0
     );
+    const [gastos, setGastos] = useState(
+        localStorage.getItem("gastos") ? JSON.parse(localStorage.getItem("gastos")) : []
+    );
     const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
     const [modal, setModal] = useState(false);
     const [animarModal, setAnimarModal] = useState(false);
-    const [gastos, setGastos] = useState([]);
     const [gastoEditar, setGastoEditar] = useState({});
 
     useEffect(() => {
@@ -28,6 +30,10 @@ function App() {
     useEffect(() => {
         localStorage.setItem('presupuesto', presupuesto ?? 0);
     }, [presupuesto]);
+
+    useEffect(() => {
+        localStorage.setItem("gastos", JSON.stringify(gastos) ?? []);
+    }, [gastos]);
 
     useEffect(() => {
         const presupuestoLS = Number(localStorage.getItem("presupuesto")) ?? 0;
