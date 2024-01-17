cea543e1cfa37ac189359839b7ef2213916ee6b1
Jgarcia <josegarcia19809@gmail.com>
2024-01-16 20:20:40 -0600

    Control de presupuesto en React: Mostrando Textos en Gráfica Circular

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -34,9 +34,12 @@ const ControlPresupuesto = ({gastos, presupuesto}) => {
                 <CircularProgressbar
                     styles={buildStyles({
                         pathColor: '#3B82F6',
-                        trailColor:'#F5F5F5'
+                        trailColor: '#F5F5F5',
+                        textColor: '#3B82F6'
                     })}
-                    value={porcentaje}/>
+                    value={porcentaje}
+                    text={`${porcentaje}% Gastado`}
+                />
             </div>
             <div className="contenido-presupuesto">
                 <p>
