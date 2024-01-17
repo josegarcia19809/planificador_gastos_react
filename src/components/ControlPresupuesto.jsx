41bbcb671a187cd45e231bfa049017cb441cae12
Jgarcia <josegarcia19809@gmail.com>
2024-01-17 10:32:46 -0600

    Control de presupuesto en React: Cambiando la apariencia de Presupuesto Disponible si ya fue agotado

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -33,9 +33,9 @@ const ControlPresupuesto = ({gastos, presupuesto}) => {
             <div>
                 <CircularProgressbar
                     styles={buildStyles({
-                        pathColor: '#3B82F6',
+                        pathColor: porcentaje > 100 ? '#DC2626' : '#3B82F6',
                         trailColor: '#F5F5F5',
-                        textColor: '#3B82F6'
+                        textColor: porcentaje > 100 ? '#DC2626' : '#3B82F6',
                     })}
                     value={porcentaje}
                     text={`${porcentaje}% Gastado`}
@@ -45,7 +45,7 @@ const ControlPresupuesto = ({gastos, presupuesto}) => {
                 <p>
                     <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
                 </p>
-                <p>
+                <p className={`${disponible < 0 ? 'negativo' : ''}`}>
                     <span>Disponible: </span> {formatearCantidad(disponible)}
                 </p>
                 <p>
